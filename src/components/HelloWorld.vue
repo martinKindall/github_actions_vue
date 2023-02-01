70fb20329a3b7ff948b308322ccd9fc5bbcbcb4b
Martin <mart256@gmail.com>
2023-02-01 22:01:12 +0100

    Test repaired

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,8 +9,9 @@ defineProps({
 
 <template>
   <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
+      Like y suscríbete
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
